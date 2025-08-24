94b570ad74f5a416c4d421ea0c0eb126c5c1ba30
Ritham Sheti <sethiritham@gmail.com>
2025-08-25 00:54:07 +0530

    "Simple API calling using async and await"

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
 ﻿using System;
+
+
+
+
+class Program
+{
+
+    private static readonly HttpClient client = new HttpClient();
+    async static Task Main(string[] args)
+    {
+        Console.WriteLine("Fetching Data from the API....");
+
+        try
+        {
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+        }
+    }
+}
